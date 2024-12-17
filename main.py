4749c975f78b4f8130836b40959503b0aba307ce
Rishabhcodes65536 <jain.rishabh32768@gmail.com>
2024-12-17 23:30:39 +0530

    backward comp check

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ def allowed_file(filename):
 # Route for homepage
 @app.route('/files')
 def index():
-    return render_template('files.html')
+    return render_template('file_upload.html')
 
 
 # Combine text with file titles for indexing
